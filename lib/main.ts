8e4f21061f48c68de1b804cf8bdc1b108a306168
rhombergalessandro <rhomberg37@gmail.com>
2024-01-18 09:14:57 +0100

    removed test component

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,3 @@
-export { default as TestComponent } from "./components/public/test.component/test.component";
-
 // dev components
 export { default as RenderComponent } from "./components/private/editor.components/render.components/render.content.component/render.content.component";
 export { default as RenderScreen } from "./components/private/editor.components/render.components/render.screen.component/render.screen.conponent";
