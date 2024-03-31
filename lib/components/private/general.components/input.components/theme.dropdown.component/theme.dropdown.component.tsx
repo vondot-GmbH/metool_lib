800a70a67021045406b9bded2610652e975728ff
rhombergalessandro <rhomberg37@gmail.com>
2024-03-31 11:46:27 +0200

    improved theme dropdown

--- lib/components/private/general.components/input.components/theme.dropdown.component/theme.dropdown.component.tsx
+++ lib/components/private/general.components/input.components/theme.dropdown.component/theme.dropdown.component.tsx
@@ -1,13 +1,21 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import classNames from "classnames";
+import styles from "./theme.dropdown.component.module.scss";
 import ConfigProvider from "../../../../../config/config.provider";
-import SelectDropDown from "../select.dropdown.component/select.dropdown.component";
-import { ThemeConfig } from "../../../../../globals/interfaces/config.interface";
+import {
+  ThemeConfig,
+  ThemeOption,
+} from "../../../../../globals/interfaces/config.interface";
+import { useClickedOutside } from "../../../../../globals/helpers/hook.helper";
+import SmallText from "../../text.components/small.text.component/small.text.component";
+import RunningText from "../../text.components/running.text.component/running.text.component";
 
 type ThemeCategory = keyof ThemeConfig;
 
 interface ThemeDropdownProps {
-  onChange: (value: any) => void;
-  selectedItem: any;
+  onChange: (value: string) => void;
+  selectedItem?: string;
   label: string;
   placeholder?: string;
   disabled?: boolean;
@@ -20,42 +28,103 @@ const ThemeDropdown = ({
   selectedItem,
   label,
   placeholder = "Select a theme variable",
-  disabled,
+  disabled = false,
   className,
   category,
 }: ThemeDropdownProps) => {
-  const [themeOptions, setThemeOptions] =
-    useState<{ label: string; value: string; color?: string }[]>();
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const optionsRef = useRef<HTMLDivElement>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [themeOptions, setThemeOptions] = useState<ThemeOption[]>([]);
+  const [dropdownPosition, setDropdownPosition] = useState({ top: 0, left: 0 });
+
+  const dropdownClassName = classNames(styles.dropdown, className, {
+    [styles.disabled]: disabled,
+  });
 
   useEffect(() => {
     const configProvider = ConfigProvider.getInstance();
     const themeConfig = configProvider.getThemeVariablesForCategory(category);
-
-    const options = themeConfig?.map((option) => ({
-      label: option.label,
-      value: option.formattedValue,
-      color: category === "colors" ? option.value : undefined,
-    }));
-
-    if (options != null) {
-      setThemeOptions(options);
+    if (themeConfig) {
+      setThemeOptions(themeConfig);
     }
   }, [category]);
 
-  if (!themeOptions) {
-    return null;
-  }
+  useEffect(() => {
+    if (isOpen && dropdownRef.current) {
+      const rect = dropdownRef.current.getBoundingClientRect();
+      setDropdownPosition({
+        top: rect.bottom + window.scrollY,
+        left: rect.left + window.scrollX,
+      });
+    }
+  }, [isOpen]);
+
+  useClickedOutside(optionsRef, () => setIsOpen(false));
+
+  const handleToggleDropdown = () => !disabled && setIsOpen(!isOpen);
+
+  const handleOptionSelect = (option: ThemeOption) => {
+    onChange(option.formattedValue);
+    setIsOpen(false);
+  };
+
+  const buildOptionItem = (option: ThemeOption, handleClick?: () => void) => {
+    return (
+      <div
+        key={option.value}
+        className={styles.option}
+        onClick={handleClick ? () => handleClick() : undefined}
+      >
+        {option.value && (
+          <div
+            className={styles.colorIndicator}
+            style={{ backgroundColor: option.value }}
+          />
+        )}
+        <SmallText>{option.label}</SmallText>
+      </div>
+    );
+  };
+
+  const selectedOption = themeOptions.find(
+    (option) => option.formattedValue === selectedItem
+  );
+  const selectedItemElement = selectedOption ? (
+    buildOptionItem(selectedOption)
+  ) : (
+    <RunningText>{placeholder}</RunningText>
+  );
 
   return (
-    <SelectDropDown
-      items={themeOptions || []}
-      onChange={onChange}
-      selectedItem={selectedItem}
-      label={label}
-      placeholder={placeholder}
-      disabled={disabled}
-      className={className}
-    />
+    <div className={dropdownClassName} ref={dropdownRef}>
+      {label && (
+        <div className={styles.label}>
+          <SmallText>{label}</SmallText>
+        </div>
+      )}
+      <div className={styles.selectedItem} onClick={handleToggleDropdown}>
+        {selectedItemElement}
+      </div>
+      {isOpen &&
+        ReactDOM.createPortal(
+          <div
+            ref={optionsRef}
+            className={styles.options}
+            style={{
+              top: `${dropdownPosition.top}px`,
+              left: `${dropdownPosition.left}px`,
+              position: "fixed",
+            }}
+          >
+            {themeOptions.map((option) =>
+              buildOptionItem(option, () => handleOptionSelect(option))
+            )}
+          </div>,
+          document.body
+        )}
+    </div>
   );
 };
 
